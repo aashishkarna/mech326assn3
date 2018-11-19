7bb17bc2588e840239a3208dbafa8d20bf36b009
aashishkarna <aashishkarna96@gmail.com>
2018-11-18 21:23:58 -0800

    changed diameter, added shoulder locations

--- x2D.m
+++ x2D.m
@@ -6,26 +6,29 @@ function D = x2D(x)
     D = ones(1, n);
     
     % Standard bearing sizes available from Shigley's Table 11-2
-    bearing1_d = 10;            % bore
+    bearing1_d = 30;            % bore
+    bearing1_sh_d = 35;       % shoulder diameter d_s
+    
     bearing1_w = 39;             % width
-    bearing1_sh_d = 12.5;       % shoulder diameter d_s
     bearing1_sh_w = 10;          % shoulder width chosen (not in shigley)
     
     % Gear diameters selected from the same table
-    gear1_d = 15;               
+    gear1_d = 35; 
+    
     gear1_w = 100;
     gear1_sh_w = 300;
     
     % Note: The shoulder for gear 1 is to be 7.5-20 mm greater than bore
     % diameter. However, this shoulder is also the bore diameter for
     % bearing 2, thus only specific values are permitted (from Table 11-2)
-    bearing2_d = 25;
-    bearing2_sh_d = 30;
+    bearing2_d = 50;
+    bearing2_sh_d = 56;
     
     % Note: Same goes for gear 2. The shoulder is to be 7.5-20 mm greater
     % than bore diameter. In this case, the shoulder is the same as the
     % shoulder for bearing 2.
-    gear2_d = 20;
+    gear2_d = 45;
+    
     gear2_w = 100;
     gear2_sh_w = 25;            % shoulder width
     

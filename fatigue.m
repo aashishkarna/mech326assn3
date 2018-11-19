4b6e5cbdd5db20e53a4630e4694665f6d591d5b6
Ryan Cotsakis <ryancotsakis@gmail.com>
2018-11-18 19:24:41 -0800

    fixed ka

--- fatigue.m
+++ fatigue.m
@@ -5,12 +5,17 @@ function [nf] = fatigue (D, Sigma, x)
   % Marin Factors
   a = 4.51;
   b = -0.265;
-  ka = a*Sut^b;
+  ka = a*(460)^b;
   
   kb = (1.51*(1000 * D).^(-0.157)).*(D>0.051)+(1.24*(1000 * D).^(-0.107)).*(D<=0.051);
   Se = Se * ka * kb;
   nf = Se ./ Sigma;
+  nf = min(nf, 10);
+  
+  plot(x, nf);
+  title('n\_fatigue');
+  
   [nf, i] = min(nf);
   disp(['Fatigue safety factor of ', num2str(nf), ' at x = ', num2str(x(i))])
-  disp('Divide n_fatigue by (0.3 + 0.7 * K_t)')  
+  disp('Divide n_fatigue by (0.3 + 0.7 * K_t)')
 end
